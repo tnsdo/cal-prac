62ef8d19c85b638fb02f549ebbe5c0e6d8dcd541
tnsdo <jungeun2115@gmail.com>
2024-08-08 15:23:45 +0900

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,90 @@
 import "./App.css";
 
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
 
-import ReactLogo from "./assets/react.svg?react";
-
-const ReadTheDocs = styled.p<{
-  $color?: CSSProperties["color"];
-}>`
-  color: ${({ $color }) => $color ?? "red"};
+const Main = styled.div`
+  width: 200px;
+  height: 400px;
+  border-radius: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
 `;
 
-function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation("main");
+const But = styled.div`
+  display: grid;
+  color: white;
+  font-size: 20px;
+  background-color: #343434;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+`;
 
+const Zero = styled.div`
+  grid-column: span 2;
+  display: grid;
+  color: white;
+  font-size: 20px;
+  background-color: #343434;
+  width: 80px;
+  height: 40px;
+  border-radius: 25%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+`;
+
+const Butcon = styled(But)`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 3px;
+`;
+
+const Cal1 = styled(But)`
+  background-color: #a9a9a9;
+  font-size: 17px;
+  font-weight: 500;
+  color: black;
+`;
+const Cal = styled(But)`
+  background-color: #ff9900;
+  font-weight: 500;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src="/src/assets/react.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ReadTheDocs $color={"blue"}>
-        {t("title")}
-        <ReactLogo width={16} height={16} />
-      </ReadTheDocs>
-    </div>
+    <Main>
+      <Butcon>
+        <Cal1>AC</Cal1>
+        <Cal1>+/-</Cal1>
+        <Cal1>%</Cal1>
+        <Cal>/</Cal>
+        <But>7</But>
+        <But>8</But>
+        <But>9</But>
+        <Cal>x</Cal>
+        <But>4</But>
+        <But>5</But>
+        <But>6</But>
+        <Cal>-</Cal>
+        <But>1</But>
+        <But>2</But>
+        <But>3</But>
+        <Cal>+</Cal>
+        <Zero>0</Zero>
+        <But>.</But>
+        <Cal>=</Cal>
+      </Butcon>
+    </Main>
   );
-}
+};
 
 export default App;
